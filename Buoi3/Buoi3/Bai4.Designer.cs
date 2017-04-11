107c0dfcc1b7ca2d289136058d6e9dce21bf7a14
DangKhoa <huynhtran.dangkhoa>
2017-04-11 12:01:46 +0700

    bai 4

---
+++ Buoi3/Buoi3/Bai4.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Buoi3
+{
+    partial class Bai4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_tong_tien_thanh_toan = new System.Windows.Forms.TextBox();
+            this.txt_tong_khach_hang = new System.Windows.Forms.TextBox();
+            this.btn_tinh_tien = new System.Windows.Forms.Button();
+            this.btn_nhap_lai = new System.Windows.Forms.Button();
+            this.btn_thanh_toan = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.ckb_sinhvien = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdb_cf_den = new System.Windows.Forms.RadioButton();
+            this.rdb_cf_sua = new System.Windows.Forms.RadioButton();
+            this.rdb_cf_sua_da = new System.Windows.Forms.RadioButton();
+            this.rdb_cf_da = new System.Windows.Forms.RadioButton();
+            this.rdb_cf_kem = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAFE SINH VIÊN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.ckb_sinhvien);
+            this.panel1.Controls.Add(this.txt_name);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 219);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btn_thoat);
+            this.panel2.Controls.Add(this.btn_thanh_toan);
+            this.panel2.Controls.Add(this.btn_nhap_lai);
+            this.panel2.Controls.Add(this.btn_tinh_tien);
+            this.panel2.Controls.Add(this.txt_tong_khach_hang);
+            this.panel2.Controls.Add(this.txt_tong_tien_thanh_toan);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(0, 252);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(488, 123);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên khách hàng:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tổng khách hàng:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Tổng tiền thanh toán:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.Location = new System.Drawing.Point(177, 9);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(298, 26);
+            this.txt_name.TabIndex = 1;
+            this.txt_name.TextChanged += new System.EventHandler(this.txt_name_TextChanged);
+            // 
+            // txt_tong_tien_thanh_toan
+            // 
+            this.txt_tong_tien_thanh_toan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tong_tien_thanh_toan.Location = new System.Drawing.Point(178, 83);
+            this.txt_tong_tien_thanh_toan.Name = "txt_tong_tien_thanh_toan";
+            this.txt_tong_tien_thanh_toan.Size = new System.Drawing.Size(298, 26);
+            this.txt_tong_tien_thanh_toan.TabIndex = 2;
+            // 
+            // txt_tong_khach_hang
+            // 
+            this.txt_tong_khach_hang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tong_khach_hang.Location = new System.Drawing.Point(178, 44);
+            this.txt_tong_khach_hang.Name = "txt_tong_khach_hang";
+            this.txt_tong_khach_hang.Size = new System.Drawing.Size(298, 26);
+            this.txt_tong_khach_hang.TabIndex = 3;
+            // 
+            // btn_tinh_tien
+            // 
+            this.btn_tinh_tien.Enabled = false;
+            this.btn_tinh_tien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tinh_tien.Location = new System.Drawing.Point(24, 5);
+            this.btn_tinh_tien.Name = "btn_tinh_tien";
+            this.btn_tinh_tien.Size = new System.Drawing.Size(105, 30);
+            this.btn_tinh_tien.TabIndex = 4;
+            this.btn_tinh_tien.Text = "&Tính tiền";
+            this.btn_tinh_tien.UseVisualStyleBackColor = true;
+            // 
+            // btn_nhap_lai
+            // 
+            this.btn_nhap_lai.Enabled = false;
+            this.btn_nhap_lai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nhap_lai.Location = new System.Drawing.Point(135, 5);
+            this.btn_nhap_lai.Name = "btn_nhap_lai";
+            this.btn_nhap_lai.Size = new System.Drawing.Size(105, 30);
+            this.btn_nhap_lai.TabIndex = 5;
+            this.btn_nhap_lai.Text = "&Nhập lại";
+            this.btn_nhap_lai.UseVisualStyleBackColor = true;
+            // 
+            // btn_thanh_toan
+            // 
+            this.btn_thanh_toan.Enabled = false;
+            this.btn_thanh_toan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thanh_toan.Location = new System.Drawing.Point(246, 5);
+            this.btn_thanh_toan.Name = "btn_thanh_toan";
+            this.btn_thanh_toan.Size = new System.Drawing.Size(105, 30);
+            this.btn_thanh_toan.TabIndex = 6;
+            this.btn_thanh_toan.Text = "T&hanh toán";
+            this.btn_thanh_toan.UseVisualStyleBackColor = true;
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Enabled = false;
+            this.btn_thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thoat.Location = new System.Drawing.Point(357, 5);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(105, 30);
+            this.btn_thoat.TabIndex = 7;
+            this.btn_thoat.Text = "Th&oát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            // 
+            // ckb_sinhvien
+            // 
+            this.ckb_sinhvien.AutoSize = true;
+            this.ckb_sinhvien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckb_sinhvien.Location = new System.Drawing.Point(177, 41);
+            this.ckb_sinhvien.Name = "ckb_sinhvien";
+            this.ckb_sinhvien.Size = new System.Drawing.Size(101, 24);
+            this.ckb_sinhvien.TabIndex = 2;
+            this.ckb_sinhvien.Text = "Sinh viên?";
+            this.ckb_sinhvien.UseVisualStyleBackColor = true;
+            this.ckb_sinhvien.CheckedChanged += new System.EventHandler(this.ckb_sinhvien_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rdb_cf_kem);
+            this.groupBox1.Controls.Add(this.rdb_cf_da);
+            this.groupBox1.Controls.Add(this.rdb_cf_sua_da);
+            this.groupBox1.Controls.Add(this.rdb_cf_sua);
+            this.groupBox1.Controls.Add(this.rdb_cf_den);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(177, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 139);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn loại cafe";
+            // 
+            // rdb_cf_den
+            // 
+            this.rdb_cf_den.AutoSize = true;
+            this.rdb_cf_den.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_cf_den.Location = new System.Drawing.Point(6, 30);
+            this.rdb_cf_den.Name = "rdb_cf_den";
+            this.rdb_cf_den.Size = new System.Drawing.Size(92, 24);
+            this.rdb_cf_den.TabIndex = 0;
+            this.rdb_cf_den.TabStop = true;
+            this.rdb_cf_den.Text = "Cafe đen";
+            this.rdb_cf_den.UseVisualStyleBackColor = true;
+            // 
+            // rdb_cf_sua
+            // 
+            this.rdb_cf_sua.AutoSize = true;
+            this.rdb_cf_sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_cf_sua.Location = new System.Drawing.Point(6, 65);
+            this.rdb_cf_sua.Name = "rdb_cf_sua";
+            this.rdb_cf_sua.Size = new System.Drawing.Size(91, 24);
+            this.rdb_cf_sua.TabIndex = 1;
+            this.rdb_cf_sua.TabStop = true;
+            this.rdb_cf_sua.Text = "Cafe sữa";
+            this.rdb_cf_sua.UseVisualStyleBackColor = true;
+            // 
+            // rdb_cf_sua_da
+            // 
+            this.rdb_cf_sua_da.AutoSize = true;
+            this.rdb_cf_sua_da.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_cf_sua_da.Location = new System.Drawing.Point(6, 100);
+            this.rdb_cf_sua_da.Name = "rdb_cf_sua_da";
+            this.rdb_cf_sua_da.Size = new System.Drawing.Size(113, 24);
+            this.rdb_cf_sua_da.TabIndex = 2;
+            this.rdb_cf_sua_da.TabStop = true;
+            this.rdb_cf_sua_da.Text = "Cafe sữa đá";
+            this.rdb_cf_sua_da.UseVisualStyleBackColor = true;
+            // 
+            // rdb_cf_da
+            // 
+            this.rdb_cf_da.AutoSize = true;
+            this.rdb_cf_da.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_cf_da.Location = new System.Drawing.Point(173, 30);
+            this.rdb_cf_da.Name = "rdb_cf_da";
+            this.rdb_cf_da.Size = new System.Drawing.Size(83, 24);
+            this.rdb_cf_da.TabIndex = 3;
+            this.rdb_cf_da.TabStop = true;
+            this.rdb_cf_da.Text = "Cafe đá";
+            this.rdb_cf_da.UseVisualStyleBackColor = true;
+            // 
+            // rdb_cf_kem
+            // 
+            this.rdb_cf_kem.AutoSize = true;
+            this.rdb_cf_kem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_cf_kem.Location = new System.Drawing.Point(173, 65);
+            this.rdb_cf_kem.Name = "rdb_cf_kem";
+            this.rdb_cf_kem.Size = new System.Drawing.Size(95, 24);
+            this.rdb_cf_kem.TabIndex = 4;
+            this.rdb_cf_kem.TabStop = true;
+            this.rdb_cf_kem.Text = "Cafe kem";
+            this.rdb_cf_kem.UseVisualStyleBackColor = true;
+            // 
+            // Bai4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 375);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Bai4";
+            this.Text = "Bai4";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdb_cf_kem;
+        private System.Windows.Forms.RadioButton rdb_cf_da;
+        private System.Windows.Forms.RadioButton rdb_cf_sua_da;
+        private System.Windows.Forms.RadioButton rdb_cf_sua;
+        private System.Windows.Forms.RadioButton rdb_cf_den;
+        private System.Windows.Forms.CheckBox ckb_sinhvien;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_thoat;
+        private System.Windows.Forms.Button btn_thanh_toan;
+        private System.Windows.Forms.Button btn_nhap_lai;
+        private System.Windows.Forms.Button btn_tinh_tien;
+        private System.Windows.Forms.TextBox txt_tong_khach_hang;
+        private System.Windows.Forms.TextBox txt_tong_tien_thanh_toan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Buoi3/Buoi3/Bai4.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi3
+{
+    public partial class Bai4 : Form
+    {
+        bool isSinhVien;
+        List<Button> listBtn;
+        List<CheckBox> listCheckBox;
+
+        private void cb_CheckedChanged(object sender, EventArgs e)
+        {
+            getText(sender);
+        }
+
+        private void getText(CheckBox c)
+        {
+            Console.WriteLine(c.Text);
+        }
+
+        public Bai4()
+        {
+            InitializeComponent();
+
+            isSinhVien = false;
+            listBtn = new List<Button>();
+            foreach (Control c in panel2.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                {
+                    Button b = (Button)c;
+                    listBtn.Add(b);
+                }
+            }
+            foreach (Control c in panel2.Controls)
+            {
+                if (c.GetType() == typeof(CheckBox))
+                {
+                    CheckBox cb = (CheckBox)c;
+                    //listCheckBox.Add(cb);
+                    cb.CheckedChanged += new System.EventHandler(cb_CheckedChanged); 
+                }
+
+            }
+        }
+
+        public void enableButton()
+        {
+            foreach (Control c in panel2.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                }
+            }
+        }
+
+        private void txt_name_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txt_name.Text))
+            {
+                enableButton();
+            }
+        }
+
+        private void ckb_sinhvien_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckb_sinhvien.Checked)
+            {
+                isSinhVien = true;
+            }
+        }
+    }
+}
